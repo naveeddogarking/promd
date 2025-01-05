0927ecdab98bad9f508343c7edd68672482e69de
TheSpeedY <104565822+SamPandey001@users.noreply.github.com>
2025-01-05 15:52:00 +0530

    test

--- commands/cmd.js
+++ commands/cmd.js
@@ -1,3 +1,16 @@
+/**
+ Copyright (C) 2022.
+ Licensed under the  GPL-3.0 License;
+ You may not use this file except in compliance with the License.
+ It is supplied in the hope that it may be useful.
+ * @project_name : Secktor-Md
+ * @author : @SamPandey001 <https://github.com/SamPandey001>
+ * @description : Secktor,A Multi-functional whatsapp bot.
+ **/
+
+
+
+
 const { cmd, sck, commands,tlang } = require(__dirname + '/../lib');
 
 cmd({
